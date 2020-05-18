80e97b5a2fa1ac010c951fd532e207c272b78521
furuixing <furuixing@vipkid.com.cn>
2020-05-18 17:53:09 +0800

    build

---
+++ dist/precache-manifest.0cedf7e5aa0845f988521f61b1f1e69f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13387870182cb8fa7113",
+    "url": "/static/js/index.a52b111f.js"
+  },
+  {
+    "revision": "c5bd575fba68118ab078",
+    "url": "/static/js/chunk-vendors.b6b2f756.js"
+  },
+  {
+    "revision": "6d6e57317afdfff484b0329177ca17ab",
+    "url": "/static/img/iconfont.6d6e5731.svg"
+  },
+  {
+    "revision": "bd4fc237240e6972b8c9be2d8628e3e1",
+    "url": "/static/fonts/iconfont.bd4fc237.ttf"
+  },
+  {
+    "revision": "4b7663a5e84ac5a57d937d2ee740edc2",
+    "url": "/static/fonts/iconfont.4b7663a5.eot"
+  },
+  {
+    "revision": "13387870182cb8fa7113",
+    "url": "/static/css/index.9787bc9e.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "99f143fb72a4a4d7a64972723ec51677",
+    "url": "/index.html"
+  }
+];
